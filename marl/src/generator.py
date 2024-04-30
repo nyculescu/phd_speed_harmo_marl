a0bad3065dbac6e12e0c1dc926cb11148cfe9333
Catalin NICULESCU <cn.cata@gmail.com>
2024-04-30 16:16:55 +0100

    The project was inherited from AndreaVidali/Deep-QLearning-Agent-for-Traffic-Signal-Control and began tailoring for TM21: Speed Harmonization. Made significant changes to the SUMO config files and some adjustments to the overall project. This checkpoint runs the training and simulation processes, but with no desirable result.

---
+++ marl/src/generator.py
@@ -0,0 +1,79 @@
+import numpy as np
+import math
+
+class TrafficGenerator:
+    def __init__(self, max_steps, n_cars_generated):
+        self._n_cars_generated = n_cars_generated  # how many cars per episode
+        self._max_steps = max_steps
+        self._veh_depart = 0
+
+    def generate_routefile(self, seed):
+        """
+        Generation of the route of every car for one episode
+        """
+        np.random.seed(seed)  # make tests reproducible
+
+        # the generation of cars is distributed according to a weibull distribution
+        timings = np.random.weibull(2, self._n_cars_generated)
+        timings = np.sort(timings)
+
+        # reshape the distribution to fit the interval 0:max_steps
+        car_gen_steps = []
+        min_old = math.floor(timings[1])
+        max_old = math.ceil(timings[-1])
+        min_new = 0
+        max_new = self._max_steps
+        for value in timings:
+            car_gen_steps = np.append(car_gen_steps, ((max_new - min_new) / (max_old - min_old)) * (value - max_old) + max_new)
+
+        car_gen_steps = np.rint(car_gen_steps)  # round every value to int -> effective steps when a car will be generated
+
+        # produce the file for cars generation, one car per line
+        with open("configs/sumo/main_road_vehicles.rou.xml", "w") as routes:
+            print("""<routes>
+	<vType accel="1.0" decel="4.5" id="normal_car_s" length="5.0" minGap="2.5" maxSpeed="42" sigma="0.5" desiredMaxSpeed="36"/>
+	<vType accel="1.5" decel="4.0" id="sporty_car_s" length="5.0" minGap="2.5" maxSpeed="67" sigma="0.6" desiredMaxSpeed="42"/>
+	<vType accel="1.0" decel="4.5" id="trailer_s" length="10.0" minGap="2.5" maxSpeed="30" sigma="0.5" desiredMaxSpeed="30"/>
+	<vType accel="1.0" decel="4.5" id="coach_s" length="15.0" minGap="2.5" maxSpeed="25" sigma="0.3" desiredMaxSpeed="25" />
+
+    <route id="left_to_right" edges="LRE LRL LRS"/>
+                  """, file=routes)
+
+            """
+            The following parameters affect the operation of the friction device.
+                device.friction.stdDev: standard deviation when adding gaussian noise (default 1)
+                device.friction.offset: constant offset to apply to all friction values (default 0)
+            Reference: https://sumo.dlr.de/docs/Definition_of_Vehicles%2C_Vehicle_Types%2C_and_Routes.html#devices
+
+
+            """
+
+            veh_hazard = np.random.randint(0, car_gen_steps)
+
+            for veh_cnt, step in enumerate(car_gen_steps):
+                veh_type_int = np.random.randint(0, 4)
+                if (veh_type_int == 0):
+                    veh_type = "normal_car_s"
+                    veh_maxDepartSpeed = np.random.randint(25, 28)
+                elif (veh_type_int == 1):
+                    veh_type = "sporty_car_s"
+                    veh_maxDepartSpeed = np.random.randint(28, 32)
+                elif (veh_type_int == 2):
+                    veh_type = "trailer_s"
+                    veh_maxDepartSpeed = np.random.randint(20, 26)
+                else:
+                    veh_type = "coach_s"
+                    veh_maxDepartSpeed = np.random.randint(20, 25)
+                
+                self._veh_depart = self._veh_depart + np.random.randint(0, 20.0)
+
+                print(f"""\t<vehicle id="{veh_cnt}" \
+type="{veh_type}" \
+depart="{self._veh_depart}" \
+from="R0.E" to="R0.S" departSpeed="random" maxDepartSpeed="{veh_maxDepartSpeed}" \
+route="left_to_right" departLane="best">\
+\n\t\t<param key="has.friction.device" value="true"/>\
+\n\t</vehicle>""", file=routes)
+
+            print("""
+</routes>""", file=routes)
---
+++ marl/src/training_simulation.py
@@ -0,0 +1,220 @@
+import traci
+import numpy as np
+import random
+import timeit
+import os
+
+# phase codes based on environment.net.xml
+PHASE_NS_GREEN = 0  # action 0 code 00
+PHASE_NS_YELLOW = 1
+PHASE_NSL_GREEN = 2  # action 1 code 01
+PHASE_NSL_YELLOW = 3
+PHASE_EW_GREEN = 4  # action 2 code 10
+PHASE_EW_YELLOW = 5
+PHASE_EWL_GREEN = 6  # action 3 code 11
+PHASE_EWL_YELLOW = 7
+
+
+class Simulation:
+    def __init__(self, Model, Memory, TrafficGen, gamma, max_steps, sumo_cmd, num_states, num_actions, training_epochs):
+        self._Model = Model
+        self._Memory = Memory
+        self._TrafficGen = TrafficGen
+        self._gamma = gamma
+        self._step = 0
+        self._sumo_cmd = sumo_cmd
+        self._max_steps = max_steps
+        self._num_states = num_states
+        self._num_actions = num_actions
+        self._reward_store = []
+        self._cumulative_wait_store = []
+        self._avg_queue_length_store = []
+        self._training_epochs = training_epochs
+
+
+    def run(self, episode, epsilon):
+        """
+        Runs an episode of simulation, then starts a training session
+        """
+        start_time = timeit.default_timer()
+
+        # first, generate the route file for this simulation and set up sumo
+        self._TrafficGen.generate_routefile(seed=episode)
+        traci.start(self._sumo_cmd)
+        print("Simulating...")
+
+        # inits
+        self._step = 0
+        self._waiting_times = {}
+        self._sum_neg_reward = 0
+        self._sum_queue_length = 0
+        self._sum_waiting_time = 0
+        old_total_wait = 0
+        old_state = -1
+        old_action = -1
+
+        while self._step < self._max_steps:
+
+            # get current state of the intersection
+            current_state = self._get_state()
+
+            # calculate reward of previous action: (change in cumulative waiting time between actions)
+            # waiting time = seconds waited by a car since the spawn in the environment, cumulated for every car in incoming lanes
+            current_total_wait = self._collect_waiting_times()
+            reward = old_total_wait - current_total_wait
+
+            # saving the data into the memory
+            if self._step != 0:
+                self._Memory.add_sample((old_state, old_action, reward, current_state))
+
+            # choose the light phase to activate, based on the current state of the intersection
+            action = self._choose_action(current_state, epsilon)
+
+
+            # saving variables for later & accumulate reward
+            old_state = current_state
+            old_action = action
+            old_total_wait = current_total_wait
+
+            # saving only the meaningful reward to better see if the agent is behaving correctly
+            if reward < 0:
+                self._sum_neg_reward += reward
+            
+            self._step += 1 # update the step counter
+
+        self._save_episode_stats()
+        print("Total reward:", self._sum_neg_reward, "- Epsilon:", round(epsilon, 2))
+        traci.close()
+        simulation_time = round(timeit.default_timer() - start_time, 1)
+
+        print("Training...")
+        start_time = timeit.default_timer()
+        for _ in range(self._training_epochs):
+            self._replay()
+        training_time = round(timeit.default_timer() - start_time, 1)
+
+        return simulation_time, training_time
+
+
+    def _simulate(self, steps_todo):
+        """
+        Execute steps in sumo while gathering statistics
+        """
+        if (self._step + steps_todo) >= self._max_steps:  # do not do more steps than the maximum allowed number of steps
+            steps_todo = self._max_steps - self._step
+
+        while steps_todo > 0:
+            traci.simulationStep()  # simulate 1 step in sumo
+            self._step += 1 # update the step counter
+            steps_todo -= 1
+            queue_length = self._get_queue_length()
+            self._sum_queue_length += queue_length
+            self._sum_waiting_time += queue_length # 1 step while wating in queue means 1 second waited, for each car, therefore queue_lenght == waited_seconds
+
+
+    def _collect_waiting_times(self):
+        """
+        Retrieve the waiting time of every car in the incoming roads
+        """
+        incoming_roads = ["E2TL", "N2TL", "W2TL", "S2TL"]
+        car_list = traci.vehicle.getIDList()
+        for car_id in car_list:
+            wait_time = traci.vehicle.getAccumulatedWaitingTime(car_id)
+            road_id = traci.vehicle.getRoadID(car_id)  # get the road id where the car is located
+            if road_id in incoming_roads:  # consider only the waiting times of cars in incoming roads
+                self._waiting_times[car_id] = wait_time
+            else:
+                if car_id in self._waiting_times: # a car that was tracked has cleared the intersection
+                    del self._waiting_times[car_id] 
+        total_waiting_time = sum(self._waiting_times.values())
+        return total_waiting_time
+
+
+    def _choose_action(self, state, epsilon):
+        """
+        Decide wheter to perform an explorative or exploitative action, according to an epsilon-greedy policy
+        """
+        if random.random() < epsilon:
+            return random.randint(0, self._num_actions - 1) # random action
+        else:
+            return np.argmax(self._Model.predict_one(state)) # the best action given the current state
+
+    def _set_safe_speed_phase(self, old_action):
+        print("_set_safe_speed_phase dummy | use traci module")
+
+    def _get_queue_length(self):
+        """
+        Retrieve the number of cars with speed = 0 in every incoming lane
+        """
+        halt_L = traci.edge.getLastStepHaltingNumber("RLL") # long road
+        halt_S = traci.edge.getLastStepHaltingNumber("RLS") # stop
+        queue_length = halt_L + halt_S
+        return queue_length
+
+
+    def _get_state(self):
+        """
+        Retrieve the state of the lane from sumo, in the form of cell occupancy
+        """
+        state = np.zeros(self._num_states)
+        veh_list = traci.vehicle.getIDList()
+
+        # For now, it's a mock
+        for car_id in veh_list: 
+            veh_speed = traci.vehicle.getSpeed()
+            veh_position = traci.vehicle.getPosition()
+            state[veh_position] = 1
+        return state
+
+
+    def _replay(self):
+        """
+        Retrieve a group of samples from the memory and for each of them update the learning equation, then train
+        """
+        batch = self._Memory.get_samples(self._Model.batch_size)
+
+        if len(batch) > 0:  # if the memory is full enough
+            states = np.array([val[0] for val in batch])  # extract states from the batch
+            next_states = np.array([val[3] for val in batch])  # extract next states from the batch
+
+            # prediction
+            q_s_a = self._Model.predict_batch(states)  # predict Q(state), for every sample
+            q_s_a_d = self._Model.predict_batch(next_states)  # predict Q(next_state), for every sample
+
+            # setup training arrays
+            x = np.zeros((len(batch), self._num_states))
+            y = np.zeros((len(batch), self._num_actions))
+
+            for i, b in enumerate(batch):
+                state, action, reward, _ = b[0], b[1], b[2], b[3]  # extract data from one sample
+                current_q = q_s_a[i]  # get the Q(state) predicted before
+                current_q[action] = reward + self._gamma * np.amax(q_s_a_d[i])  # update Q(state, action)
+                x[i] = state
+                y[i] = current_q  # Q(state) that includes the updated action value
+
+            self._Model.train_batch(x, y)  # train the NN
+
+
+    def _save_episode_stats(self):
+        """
+        Save the stats of the episode to plot the graphs at the end of the session
+        """
+        self._reward_store.append(self._sum_neg_reward)  # how much negative reward in this episode
+        self._cumulative_wait_store.append(self._sum_waiting_time)  # total number of seconds waited by cars in this episode
+        self._avg_queue_length_store.append(self._sum_queue_length / self._max_steps)  # average number of queued cars per step, in this episode
+
+
+    @property
+    def reward_store(self):
+        return self._reward_store
+
+
+    @property
+    def cumulative_wait_store(self):
+        return self._cumulative_wait_store
+
+
+    @property
+    def avg_queue_length_store(self):
+        return self._avg_queue_length_store
+
